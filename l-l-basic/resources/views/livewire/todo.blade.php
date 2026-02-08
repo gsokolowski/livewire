23874dd159ed149e3abc733b9b7edbeb1c9108b7
gsokolowski <gregor.sokolowski@gmail.com>
2026-02-08 14:59:04 +0100

    todos better

--- l-l-basic/resources/views/livewire/todo.blade.php
+++ l-l-basic/resources/views/livewire/todo.blade.php
@@ -1,10 +1,29 @@
-<div>
-    <h1>Todo List</h1>
-    <ul>
-        @foreach ($todos as $todo)
-            <li>{{ $todo }} <button wire:click="removeTodo({{ $loop->index }})">Remove</button></li>
-        @endforeach
-    </ul>
-    <input type="text" wire:model="todo" placeholder="Add a new todo">
-    <button wire:click="addTodo">Add</button>
+<div class="max-w-md mx-auto p-6 bg-gray-100 rounded-lg">
+    <h1 class="text-2xl font-bold mb-4 text-center">Todo List</h1>
+    
+    <div class="space-y-4">
+        <ul class="space-y-2">
+            @foreach ($todos as $todo)
+                <li class="flex items-center justify-between bg-white p-3 rounded shadow">
+                    <span>{{ $todo }}</span>
+                    <button 
+                        wire:click="removeTodo({{ $loop->index }})"
+                        class="text-red-500 hover:text-red-700 hover:bg-red-50 p-2 rounded transition-colors"
+                        title="Delete todo"
+                    >
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </li>
+            @endforeach
+        </ul>
+        
+        
+        <input class="w-full border rounded px-4 py-2"
+        type="text" 
+        wire:model="todo"
+        placeholder="Add a new todo"
+        wire:keydown.enter="addTodo"
+        >
+        <button wire:click="addTodo" class="bg-blue-500 text-white px-4 py-2 rounded">Add</button>
+    </div>
 </div>
