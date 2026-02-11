6d6fc88f4de3f9c3c7cd18e6b62e5ecba055fb8c
gsokolowski <gregor.sokolowski@gmail.com>
2026-02-11 03:07:27 +0100

    Add Saving... on buttons and save and save updates

--- l-l-crud/resources/views/livewire/create-student.blade.php
+++ l-l-crud/resources/views/livewire/create-student.blade.php
@@ -116,9 +116,12 @@
                                     class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-4">
                                     Cancel
                                 </a>
-                                <button type="submit"
-                                    class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                    Save
+                                <button type="submit" 
+                                    class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center items-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    <span wire:loading.remove wire:target="createStudent">Save</span>
+                                    <span wire:loading wire:target="createStudent" class="flex items-center">
+                                        Saving...
+                                    </span>
                                 </button>
                             </div>
                         </div>
--- l-l-crud/resources/views/livewire/update-student.blade.php
+++ l-l-crud/resources/views/livewire/update-student.blade.php
@@ -81,7 +81,10 @@
                                 </a>
                                 <button type="submit"
                                     class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                    Save Changes
+                                    <span wire:loading.remove wire:target="updateStudent">Save Changes</span>
+                                    <span wire:loading wire:target="updateStudent" class="flex items-center">
+                                        Saving...
+                                    </span>
                                 </button>
                             </div>
                         </div>
