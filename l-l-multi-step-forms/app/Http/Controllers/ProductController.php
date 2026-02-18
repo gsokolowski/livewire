f3bfc3b6a481b9e076d79d4bddf42b5f8961a8fb
gsokolowski <gregor.sokolowski@gmail.com>
2026-02-18 19:22:36 +0100

    Add list of products

--- l-l-multi-step-forms/app/Http/Controllers/ProductController.php
+++ l-l-multi-step-forms/app/Http/Controllers/ProductController.php
@@ -4,9 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
+    /**
+     * Display a listing of the products.
+     */
+    public function index(): View
+    {
+        $products = Product::with('user')
+            ->where('user_id', Auth::id())
+            ->latest()
+            ->get();
+
+        return view('products.index', [
+            'products' => $products,
+        ]);
+    }
+
     /**
      * Display the specified product.
      */
--- l-l-multi-step-forms/routes/web.php
+++ l-l-multi-step-forms/routes/web.php
@@ -17,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    
     Route::get('/products/create', function () {
         return view('product.create');
     })->name('products.create');
